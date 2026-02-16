3cdc2af851c3ad8f46f6cec45526a66b64739f2d
Exerny1 <rodrigo.coolkid@gmail.com>
2026-02-16 01:16:56 -0500

    Update config.js

--- public/javascript/config.js
+++ public/javascript/config.js
@@ -8,7 +8,7 @@ export let config = {
     "staticIcons": false,
     "transparentLDL": 0,
     "ldlClock": true,
-    "presentationType": 2,
+    "presentationType": 1,
     "videoType": 1,
     "farenheightInMetricUnits": true, // Force the use of farenheight for temperature whenever metric units are used (coming soon)
     "textureFiltering": true,
